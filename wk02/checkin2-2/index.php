08a45662e98c6820ab06748a36d67e1ebb452313
emwing <emwing@emily-wing.com>
2018-01-25 02:28:45 -0500

    final commit check in 2.2

--- wk02/checkin2-2/index.php
+++ wk02/checkin2-2/index.php
@@ -7,6 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
+    <?php
+    // define the array
+      $myDogSuki = array("Suki","Shiba Inu","white",6,22.5);
 
+    // define the function
+      function walkTheDog($arrayToList){
+        echo "<ul>";
+        foreach ($arrayToList as $value) {
+          echo "<li>$value</li>";
+        }
+        echo "</ul>";
+      }
+
+    // run the function with the array specified
+      walkTheDog($myDogSuki);
+    ?>
   </body>
 </html>
