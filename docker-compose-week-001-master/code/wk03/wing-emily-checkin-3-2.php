a653b400d7fa46ed905d22d667d6ca995a45c738
emwing <emwing@emily-wing.com>
2018-01-30 13:42:18 -0500

    initial commit

---
+++ docker-compose-week-001-master/code/wk03/wing-emily-checkin-3-2.php
@@ -0,0 +1,22 @@
+<?php
+
+  echo "<h1>Check-In 3.2</h1> <br />";
+
+  $queryArray = $_GET;
+
+    // foreach ($queryArray as $keyName => $value) {
+    //   echo $keyName;
+    //   echo $value;
+    // }
+
+    foreach ($queryArray as $keyStatement => $bool) {
+
+      if ($bool == false) {
+        echo "The variable, " . $keyStatement . ", has a value of false";
+      } else {
+        echo "The variable, " . $keyStatement . ", has a value of true";
+      }
+
+    }
+
+?>
