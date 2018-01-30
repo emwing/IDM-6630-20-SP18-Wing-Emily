de9070dfa7722163758fc32f2b8cd3273213dab5
emwing <emwing@emily-wing.com>
2018-01-30 01:58:18 -0500

    initial commit

---
+++ docker-compose-week-001-master/code/wk03/wing-emily-checkin-3-1.php
@@ -0,0 +1,15 @@
+<?php
+
+  echo "<h1>Check-In 3.1</h1> <br />";
+  echo "<ul>";
+
+  $queryArray = $_GET;
+
+    foreach ($queryArray as $keyName => $value) {
+      echo "<li>" . $keyName . ": ";
+      echo $value . "</li>";
+    }
+
+  echo "</ul>";
+
+?>
